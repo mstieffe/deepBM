203dfd5caeeb6ff04007070b14d28186deac2928
Marc Stieffenhofer <mstieffe@students.uni-mainz.de>
2021-01-08 17:55:53 +0100

    sPS data

--- dbm/data.py
+++ dbm/data.py
@@ -54,6 +54,7 @@ class Data():
 
         #find maximums for padding
         self.max = self.get_max_dict()
+        print("max atoms: ", max['atoms_loc_env'])
 
         print("Successfully created universe! This took ", timer()-start, "secs")
 
