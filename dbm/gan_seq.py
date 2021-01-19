ad0b38ec01cf29c3d4e4bb14a50bfc18653f3a89
Marc Stieffenhofer <mstieffe@students.uni-mainz.de>
2021-01-19 14:29:40 +0100

    val

--- dbm/gan_seq.py
+++ dbm/gan_seq.py
@@ -778,6 +778,7 @@ class GAN_SEQ():
                         for c, a in zip(new_coords, d['atom_seq']):
 
                             a.pos = d['loc_env'].rot_back(c)
+                            a.ref_pos = d['loc_env'].rot_back(c)
 
                 print(timer()-start)
                 stats.evaluate(train=False, subdir=str(self.epoch)+str(g), save_samples=True)
